d2aedd6257c1dc0f47f92cb34da74a38b361d3e6
Trevor Lewis <tenzingtitan@gmail.com>
2019-03-09 11:31:37 -0700

    Binary Search Tress passes all tests

--- search/binary_search_tree.py
+++ search/binary_search_tree.py
@@ -5,10 +5,40 @@ class BinarySearchTree:
     self.right = None
 
   def depth_first_for_each(self, cb):
-    pass    
+    # get a reference to right and left nodes
+    left = self.left
+    right = self.right
+    #return cb with a value
+    cb(self.value) 
+
+    # if left exists, recur until we hit the bottom
+    if left is not None:
+      left.depth_first_for_each(cb)
+    # if right exists, recur until we hit the bottom
+    if right is not None:
+      right.depth_first_for_each(cb)
 
   def breadth_first_for_each(self, cb):
-    pass
+    # get reference to left and right
+    left = self.left
+    right = self.right
+    # return cb with a value
+    cb(self.value)
+
+    if left is not None:
+      cb(left.value)
+
+    if right is not None:
+      cb(right.value)
+
+    if left and left.left is not None:
+      left.left.breadth_first_for_each(cb)
+    if left and left.right is not None:
+      left.right.breadth_first_for_each(cb)
+    if right and right.left is not None:
+      right.left.breadth_first_for_each(cb)
+    if right and right.right is not None:
+      right.right.breadth_first_for_each(cb)
 
   def insert(self, value):
     new_tree = BinarySearchTree(value)
